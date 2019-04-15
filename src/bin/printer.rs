741371364d02f05e14a4990bd86b28be817fb3e6
Francis Begyn <francis.begyn@gmail.com>
2019-04-16 01:04:51 +0200

    move from main to seperate binary

---
+++ src/bin/printer.rs
@@ -0,0 +1,18 @@
+extern crate inventorer_print;
+
+use self::inventorer_print::*;
+//use std::io;
+
+fn main(){
+    //println!("Barcode:");
+    //let mut barcode = String::new();
+    //io::stdin()
+    //    .read_line(&mut barcode)
+    //    .expect("failed to read from stdio");
+    //let barcode = barcode.trim();
+
+    //barcode_to_ean13(barcode, "/tmp/barcode.png");
+    barcode_to_ean13("012345678901", "/tmp/barcode.png");
+    render_small_label("schroevendraaier",  "rek 5",  "site&safety", "012345678901", "/tmp/barcode.png", "/tmp/label.pdf");
+    //print_barcode("/tmp/label.pdf");
+}
